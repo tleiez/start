829d29ed00fb0ac94a2dee63e05226db777f4411
tleiez <145976778+tleiez@users.noreply.github.com>
2023-10-02 19:49:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const name = 'Andrey'; 
+let age = 20;
+const after_5_years = 20**2 / 5 * 2 - 135;
+const from = 'no data available';
+
+const USER = {
+    name,
+    age,
+    after_5_years,
+    from
+};
+console.log(USER);
+
+const g = 3;
+const h = '54a';
+const m = null;
+
+const MATH = {
+    a: 5+5,
+    b: 5-5,
+    c: 5*5,
+    d: 5/5,
+    e: 37 && 5,
+    f: 5**2,
+    i: g+h,
+    j: Number(g)+Number(h),
+    k: 84 % 5,
+    l: true + 5,
+    n: true * m
+};
+console.log(MATH);
